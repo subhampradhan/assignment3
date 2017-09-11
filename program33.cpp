62c1816c11432be84efbe7c5f4df526c3fa71465
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-09-11 08:02:44 +0530

    Create program33.cpp

---
+++ program33.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,last,first;
+	cin>>i;
+	while(i>10)
+	{
+	 first=i/10;
+	}
+	cout<<"first digit is "<<first<<endl;
+	cout<<"last digit is "<<i%10<<endl;
+	cout<<"sum of first and last digit is "<<first+last<<endl;
+	
+	
+	return 0;
+}
