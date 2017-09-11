9f423a1936c225fdf0889a574da294c5014221ee
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-09-11 07:57:37 +0530

    Create program32.cpp

---
+++ program32.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,last,first;
+	cin>>i;
+	while(i>10)
+	{
+	 first=i/10;
+	}
+	cout<<"first digit is "<<first<<endl;
+	cout<<"last digit is "<<i%10<<endl;
+	return 0;
+}
