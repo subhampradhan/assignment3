0e25f7b8be2479114e356cf8fbdf9b0c2725ed89
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-09-11 15:19:01 +0530

    Update program33.cpp

--- program33.cpp
+++ program33.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-	int i,last,first;
+	int i,x;
 	cin>>i;
+	x=i;
 	while(i>10)
 	{
-	 first=i/10;
+	 i=i/10;
 	}
-	cout<<"first digit is "<<first<<endl;
-	cout<<"last digit is "<<i%10<<endl;
-	cout<<"sum of first and last digit is "<<first+last<<endl;
+	cout<<"first digit is "<<i<<endl;
+	cout<<"last digit is "<<x%10<<endl;
+	cout<<"sum of first and last digit is "<<(x%10)+i<<endl;
 	
 	
 	return 0;
