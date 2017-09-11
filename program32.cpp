018e0d23714e6158fbee0a1fdf5f3c486c06a4a9
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-09-11 15:10:58 +0530

    Update program32.cpp

--- program32.cpp
+++ program32.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-	int i,last,first;
+	int i,last,x;
 	cin>>i;
+	x=i;
 	while(i>10)
 	{
-	 first=i/10;
+	 i=i/10;
 	}
-	cout<<"first digit is "<<first<<endl;
-	cout<<"last digit is "<<i%10<<endl;
+	cout<<"first digit is "<<i<<endl;
+	cout<<"last digit is "<<(x%10)<<endl;
 	return 0;
 }
